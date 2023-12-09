35d44e4ba2422b2e92da64d27d6de6c7b6ae31c4
Justin <jgarey02@gmail.com>
2023-12-09 16:01:38 -0500

    Day 9 is done and is my best on the leaderboards so far! 43,516the place today!

---
+++ 2023/day9/solution2.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+DEBUG = 0
+
+with open("input1.in", "r") as file:
+    sum = 0
+    for line in file.readlines():
+        sequences = [[]]
+        sequences[0] = [int(x) for x in line.strip().split()[::-1]]
+        if DEBUG:
+            print(f"History is {sequences[0]}")
+        while True:
+            prev_x = sequences[-1][0]
+            new_sequence = []
+            for x in sequences[-1][1:]:
+                new_sequence.append(x - prev_x)
+                prev_x = x
+            sequences.append(new_sequence)
+            if DEBUG:
+                print(f"New sequence {new_sequence}")
+            if len(set(new_sequence)) <= 1:
+                break
+        sequences[-1].append(sequences[-1][0])
+        sequences.reverse()
+        for index in range(len(sequences) - 1):
+            sequences[index + 1].append(sequences[index][-1] + sequences[index + 1][-1])
+        sum += sequences[-1][-1]
+    print(sum)
